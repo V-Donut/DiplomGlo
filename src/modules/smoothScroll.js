980f9ff5c7f85ec07f852f02675cf6f60a36c1fe
V-Donut <vikaevt123@gmail.com>
2022-03-20 21:42:04 +0300

    Плавная прокрутка

--- src/modules/smoothScroll.js
+++ src/modules/smoothScroll.js
@@ -1,5 +1,24 @@
 const smoothScroll = () => {
-  
+  const scrollBtn = document.querySelector('.smooth-scroll');
+  const firstSection = document.querySelector('.section');
+  const sectionCoordinates = firstSection.getBoundingClientRect();
+
+  scrollBtn.style.display = 'none';
+
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset > sectionCoordinates.top) {
+      scrollBtn.style.display = 'block';
+    } else {
+      scrollBtn.style.display = 'none';
+    }
+  });
+
+  scrollBtn.addEventListener('click', () => {
+    document.querySelector('#header').scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
 };
 
 export default smoothScroll;
