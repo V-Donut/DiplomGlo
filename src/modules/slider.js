e8c6351cfa222b31e5f9a231d4a8c99c48734430
V-Donut <vikaevt123@gmail.com>
2022-03-21 14:53:21 +0300

    Наши услуги

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,5 +1,30 @@
+import Swiper, { Navigation, Autoplay } from 'swiper';
+
 const slider = () => {
- 
+  const swiper = new Swiper('.our-services', {
+    slidesPerView: 2,
+    spaceBetween: 40,
+    loop: true,
+    modules: [Navigation, Autoplay],
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.services__arrow--right',
+      prevEl: '.services__arrow--left',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      }
+    }
+  });
 };
 
 export default slider;
