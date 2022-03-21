5b92f0a7940b4024d30daf4db8e9bf010a3b4fba
V-Donut <vikaevt123@gmail.com>
2022-03-21 20:48:05 +0300

    Выгода для клиентов

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,7 +1,7 @@
 import Swiper, { Navigation, Autoplay } from 'swiper';
 
 const slider = () => {
-  const swiper = new Swiper('.our-services', {
+  const servicesSlider = new Swiper('.our-services', {
     slidesPerView: 2,
     spaceBetween: 40,
     loop: true,
@@ -22,6 +22,28 @@ const slider = () => {
       576: {
         slidesPerView: 2,
         spaceBetween: 40,
+      }
+    }
+  });
+
+  const benefitsSlider = new Swiper('.our-benefits', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    slidesPerColumnFill: 'row',
+    loop: true,
+    modules: [Navigation],
+    navigation: {
+      nextEl: '.benefits__arrow--right',
+      prevEl: '.benefits__arrow--left',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      576: {
+        slidesPerView: 3,
+        spaceBetween: 212,
       }
     }
   });
